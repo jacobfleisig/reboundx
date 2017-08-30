21653d431690db35e8606fe02d83712e0236b497
Jake Fleisig <jafl7087@colorado.edu>
2017-08-30 10:37:06 -0600

    Add files via upload

---
+++ src/modify_orbits_direct.c
@@ -0,0 +1,74 @@
+/**
+ * @file    modify_orbits_direct.c
+ * @brief   Update orbital with prescribed timescales by directly changing orbital elements after each timestep.
+ * @author  Dan Tamayo <tamayo.daniel@gmail.com>
+ * 
+ * @section     LICENSE
+ * Copyright (c) 2015 Dan Tamayo, Hanno Rein
+ *
+ * This file is part of reboundx.
+ *
+ * reboundx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * reboundx is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rebound.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The section after the dollar signs gets built into the documentation by a script.  All lines must start with space * space like below.
+ * Tables always must be preceded and followed by a blank line.  See http://docutils.sourceforge.net/docs/user/rst/quickstart.html for a primer on rst.
+ * $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+ *
+ * $Orbit Modifications$       // Effect category (must be the first non-blank line after dollar signs and between dollar signs to be detected by script).
+ *
+ * ======================= ===============================================
+ * Authors                 D. Tamayo
+ * Implementation Paper    *In progress*
+ * Based on                `Lee & Peale 2002 <http://labs.adsabs.harvard.edu/adsabs/abs/2002ApJ...567..596L/>`_. 
+ * C Example               :ref:`c_example_modify_orbits`
+ * Python Example          `Migration.ipynb <https://github.com/dtamayo/reboundx/blob/master/ipython_examples/Migration.ipynb>`_,
+ *                         `EccAndIncDamping.ipynb <https://github.com/dtamayo/reboundx/blob/master/ipython_examples/EccAndIncDamping.ipynb>`_.
+ * ======================= ===============================================
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "rebound.h"
+#include "reboundx.h"
+
+static struct reb_particle rebx_calculate_modify_orbits_direct(struct reb_simulation* const sim, struct rebx_effect* const effect, struct reb_particle* p, struct reb_particle* primary, const double dt){
+	int err=0;
+	struct reb_orbit o = reb_tools_particle_to_orbit_err(sim->G, *p, *primary, &err);
+	double time = sim->t;
+
+	if(err){        // mass of primary was 0 or p = primary.  Return same particle without doing anything.
+		return *p;
+	}
+
+	double d_a = 0.1;
+
+	// if integer ratio of 2pi, add a kick in semi-major axis	
+	if (time/M_PI - (int)(time/M_PI) == 0){
+    		o.a += d_a;
+	}
+	return reb_tools_orbit_to_particle(sim->G, *primary, p->m, o.a, o.e, o.inc, o.Omega, o.omega, o.f);
+}
+
+void rebx_modify_orbits_direct(struct reb_simulation* const sim, struct rebx_effect* const effect, const double dt, enum rebx_timing timing){
+	const int* const ptr = rebx_get_param_check(effect, "coordinates", REBX_TYPE_INT);
+	enum REBX_COORDINATES coordinates = REBX_COORDINATES_JACOBI;
+	if (ptr != NULL){
+		coordinates = *ptr;
+	}
+	const int back_reactions_inclusive = 1;
+	const char* reference_name = "primary";
+	rebxtools_com_ptm(sim, effect, coordinates, back_reactions_inclusive, reference_name, rebx_calculate_modify_orbits_direct, dt);
+}
